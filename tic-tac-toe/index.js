b87bd6748eee84ff3d1b5f34727dc8d775a50dbc
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2025-01-16 17:05:25 -0500

    validar pocisiones

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -13,21 +13,51 @@ listFields.forEach((element) => {
     if(element.style.backgroundColor === ''){
       if (currentPlayer === player1) {
         element.style.backgroundColor = "red";
-        listFields.forEach((element) => {
-            currentPositions.push(element.style.backgroundColor)      
-        })
+        saveCurrentPosition()
         currentPlayer = player2;
       } else {
         element.style.backgroundColor = "blue";
-        listFields.forEach((element) => {
-            currentPositions.push(element.style.backgroundColor)           
-        })
+        saveCurrentPosition()
         currentPlayer = player1;
       }
     }
+    findWinner()
   });
 });
-listFields.forEach((element) => {
-    currentPositions.push(element.style.backgroundColor)  
-})
-console.log(currentPositions)
+
+function saveCurrentPosition() {
+    const positions = currentPositions.map((element) => element.posicion)
+    listFields.forEach((element, index) => {
+        if(element.style.backgroundColor !== '' && !positions.includes(index)){
+            currentPositions.push({color: element.style.backgroundColor, posicion: index})
+        }
+    })
+}
+
+function findWinner() {
+    if(currentPositions.length >= 5){
+        const fieldPlayer1 = currentPositions.map(element => {
+            return element.color === 'red' ? element.posicion : null
+        }).filter(element => element !== null)
+        const fieldPlayer2 = currentPositions.map(element => {
+            return element.color === 'blue' ? element.posicion : null
+        }).filter(element => element !== null)
+    }
+}
+
+// const winner = [
+//     [[0,0],[0,1],[0,2]],
+//     [[1,0],[1,1],[1,2]],
+//     [[2,0],[2,1],[2,2]],
+//     [[0,0],[1,0],[2,0]],
+//     [[0,1],[1,1],[2,1]],
+//     [[0,2],[1,2],[2,2]],
+//     [[0,0],[1,1],[2,2]],
+//     [[0,2],[1,1],[2,0]]
+// ]
+
+const winnerPositions = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+]
