a032053ac60432abb804aaacc7ce71a2a846ff04
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2025-01-19 22:55:11 -0500

    Solucion comentarios y agregar estilos

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,15 +1,30 @@
 const $ = (el) => document.querySelector(el);
 const $$ = (els) => document.querySelectorAll(els);
+const $$$ = (el, cl) => el.classList.add(cl);
+const $$$$ = (el, cl) => el.getAttribute(cl);
+const fieldColor = () =>
+  currentPlayer === "X" ? "backgroung-pine" : "backgroung-fields";
 
-const playerOne = 1;
-const playerTwo = 2;
-let currentPlayer = playerOne;
-let winner = 0;
-let playerOneColor = 'red'
-let playerTwoColor = 'blue'
+let playerOne = "";
+let playerTwo = "";
+let currentPlayer = "";
+let winner = "";
+let currentPositions = [];
+
+let buttons = $$(".buttons");
+
+buttons.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    if (!playerOne) {
+      playerOne = e.target.value;
+      playerTwo = playerOne === "X" ? "O" : "X";
+      currentPlayer = playerOne;
+      showCurrentShift();
+    }
+  });
+});
 
 const listFields = $$(".field");
-let currentPositions = [];
 const winnerPositions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -21,14 +36,14 @@ const winnerPositions = [
   [2, 4, 6],
 ];
 
-const currentShift = $('.shift')
+const currentShift = $(".shift");
 
 function showCurrentShift() {
-  if(!winner){
-    currentShift.innerText = `Turno Actual: ${currentPlayer}`
+  if (!winner && playerOne && currentPositions.length !== 9) {
+    currentShift.innerText = `Turno: ${currentPlayer}`;
   }
 }
-showCurrentShift() 
+showCurrentShift();
 
 listFields.forEach((element) => {
   //TODO: Sacar toda esta funcion aparte. (SOLUCIONADO)
@@ -36,47 +51,51 @@ listFields.forEach((element) => {
 });
 
 function paintField(element) {
-  if (element.style.backgroundColor === "" && !winner) {
+  const fieldClass = $$$$(element, "class");
+  if (fieldClass.length === 5 && !winner && playerOne) {
+    const color = fieldColor();
+    $$$(element, color);
+    element.innerText = currentPlayer;
+    saveCurrentPosition();
+    findWinner();
     if (currentPlayer === playerOne) {
-      element.style.backgroundColor = playerOneColor;
-      saveCurrentPosition();
       currentPlayer = playerTwo;
-      findWinner(playerOneColor, playerOne);
     } else {
-      element.style.backgroundColor = playerTwoColor;
-      saveCurrentPosition();
       currentPlayer = playerOne;
-      findWinner(playerTwoColor, playerTwo);
     }
     // TODO: Al enviar un pr nunca se pasa con console.log() (SOLUCIONADO)
   }
-  showCurrentShift()
-
+  showCurrentShift();
 }
 
 function saveCurrentPosition() {
   const positions = currentPositions.map((element) => element.posicion);
   listFields.forEach((element, index) => {
-    if (element.style.backgroundColor !== "" && !positions.includes(index)) {
+    const fieldClass = $$$$(element, "class");
+    if (fieldClass.length !== 5 && !positions.includes(index)) {
       currentPositions.push({
-        color: element.style.backgroundColor,
+        player: currentPlayer,
         posicion: index,
       });
     }
   });
 }
 
-function findWinner(color, player) {
+function getFieldsCurrentPlayer() {
+  return currentPositions
+    .map((element) => {
+      return element.player === currentPlayer ? element.posicion : null;
+    })
+    .filter((element) => element !== null);
+}
+
+function findWinner() {
   if (currentPositions.length >= 5) {
     // TODO: Hay una manera mas optima de revisar los jugadores,
     // aca esta validando siempre los dos jugadores, quiero que ahora solo valide si el jugador actual esta jugando
     // ya gano pero solo valide un jugador. (SOLUCIONADO)
 
-      const fieldsCurrentPlayer = currentPositions
-      .map((element) => {
-        return element.color === color ? element.posicion : null;
-      })
-      .filter((element) => element !== null);
+    const fieldsCurrentPlayer = getFieldsCurrentPlayer();
 
     for (let index = 0; index < winnerPositions.length; index++) {
       const element = winnerPositions[index];
@@ -84,20 +103,22 @@ function findWinner(color, player) {
         fieldsCurrentPlayer.length >= element.length &&
         element.every((field) => fieldsCurrentPlayer.includes(field))
       ) {
-        winner = player
+        winner = currentPlayer;
       }
     }
-    if (winner) winner === 1 ? showWinner('Gano Jugador X') : showWinner('Gano Jugador O');
-    if (!winner && currentPositions.length === 9) showWinner('Empate')
+    if (winner) showWinner(`Gano Jugador ${currentPlayer}`);
+    if (!winner && currentPositions.length === 9) showWinner("Empate");
   }
 }
 
 function showWinner(text) {
-  return setTimeout(() => alert(text))
+  // return setTimeout(() => alert(text), 100) 
+  const title = $(".winner");
+  title.innerText = text;
 }
 
 /*
  * TODO: Como debe eliminar el console.log ahora quiero que en la pantalla del usuario le muestre si ya gano o si es empate. (SOLUCIONADO)
  * TODO: Tambien quiero que en la pantalla del usuario ahora muestre el turno del jugador actual. (En estos momentos solo se el turno de quien es al darle a alguna casilla y me pone el color). (SOLUCIONADO)
- * TODO: Cambiar el color por el X o el O y que el usuario pueda seleccionar cual quiere.
+ * TODO: Cambiar el color por el X o el O y que el usuario pueda seleccionar cual quiere. (SOLUCIONADO)
  */
