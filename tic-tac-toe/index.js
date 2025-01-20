bea6af40b274de7e7e65fd01b460406e048acf53
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2025-01-20 09:04:57 -0500

    Eliminar comentarios

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -46,7 +46,6 @@ function showCurrentShift() {
 showCurrentShift();
 
 listFields.forEach((element) => {
-  //TODO: Sacar toda esta funcion aparte. (SOLUCIONADO)
   element.addEventListener("click", () => paintField(element));
 });
 
@@ -63,7 +62,6 @@ function paintField(element) {
     } else {
       currentPlayer = playerOne;
     }
-    // TODO: Al enviar un pr nunca se pasa con console.log() (SOLUCIONADO)
   }
   showCurrentShift();
 }
@@ -91,20 +89,13 @@ function getFieldsCurrentPlayer() {
 
 function findWinner() {
   if (currentPositions.length >= 5) {
-    // TODO: Hay una manera mas optima de revisar los jugadores,
-    // aca esta validando siempre los dos jugadores, quiero que ahora solo valide si el jugador actual esta jugando
-    // ya gano pero solo valide un jugador. (SOLUCIONADO)
-
     const fieldsCurrentPlayer = getFieldsCurrentPlayer();
-
-    
     for (let index = 0; index < winnerPositions.length; index++) {
       const element = winnerPositions[index];
-      const isWinnerPositions = element.every((field) => fieldsCurrentPlayer.includes(field))
-      if (
-        fieldsCurrentPlayer.length >= element.length &&
-        isWinnerPositions
-      ) {
+      const isWinnerPositions = element.every((field) =>
+        fieldsCurrentPlayer.includes(field)
+      );
+      if (fieldsCurrentPlayer.length >= element.length && isWinnerPositions) {
         winner = currentPlayer;
       }
     }
@@ -114,13 +105,6 @@ function findWinner() {
 }
 
 function showWinner(text) {
-  // return setTimeout(() => alert(text), 100) 
   const title = $(".winner");
   title.innerText = text;
 }
-
-/*
- * TODO: Como debe eliminar el console.log ahora quiero que en la pantalla del usuario le muestre si ya gano o si es empate. (SOLUCIONADO)
- * TODO: Tambien quiero que en la pantalla del usuario ahora muestre el turno del jugador actual. (En estos momentos solo se el turno de quien es al darle a alguna casilla y me pone el color). (SOLUCIONADO)
- * TODO: Cambiar el color por el X o el O y que el usuario pueda seleccionar cual quiere. (SOLUCIONADO)
- */
