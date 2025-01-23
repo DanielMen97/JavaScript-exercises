95c5e95a4b73354cb614ea4cb95db33d2ea67343
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2025-01-22 23:18:19 -0500

    Operaciones y borrar

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,76 @@
-const darkModeBtn = document.querySelector('.dark')
-const ligthModeBtn = document.querySelector('.ligth')
-const galaxyModeBtn = document.querySelector('.galaxy')
+const darkModeBtn = document.querySelector(".dark");
+const ligthModeBtn = document.querySelector(".ligth");
+const galaxyModeBtn = document.querySelector(".galaxy");
 
-darkModeBtn.addEventListener('click', () => setUserTheme('dark'))
-ligthModeBtn.addEventListener('click', () => setUserTheme('ligth'))
-galaxyModeBtn.addEventListener('click', () => setUserTheme('galaxy'))
+darkModeBtn.addEventListener("click", () => setUserTheme("dark"));
+ligthModeBtn.addEventListener("click", () => setUserTheme("ligth"));
+galaxyModeBtn.addEventListener("click", () => setUserTheme("galaxy"));
 
 function setUserTheme(theme) {
-   document.documentElement.setAttribute("data-theme", theme)
+  document.documentElement.setAttribute("data-theme", theme);
 }
 
+const operations = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
 
+const $ = (el) => document.querySelector(el);
+const $$ = (els) => document.querySelectorAll(els);
+
+const display = $(".calc__display-text");
+const numberButtons = $$(".numbers");
+const operationButtons = $$(".operations");
+const equalButton = $(".equals");
+const deleteButton = $(".delete");
+const resetButton = $(".reset");
+
+let valueOne = "";
+let valueTwo = "";
+let currentOperation = "";
+
+numberButtons.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    const value = e.target.value;
+    if (currentOperation) {
+      valueTwo = valueTwo + value;
+      display.innerText = valueTwo;
+    } else {
+      valueOne = valueOne + value;
+      display.innerText = valueOne;
+    }
+  });
+});
+
+operationButtons.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    if (valueOne) {
+      currentOperation = e.target.value;
+    }
+  });
+});
+
+equalButton.addEventListener("click", function () {
+  if (valueOne && valueTwo && currentOperation) {
+    const result = operations[currentOperation](
+      Number(valueOne),
+      Number(valueTwo)
+    );
+    display.innerText = result;
+    valueOne = result.toString();
+    valueTwo = "";
+    currentOperation = "";
+  }
+});
+
+deleteButton.addEventListener("click", function () {
+  if (currentOperation) {
+    valueTwo = valueTwo.slice(0, valueTwo.length - 1) 
+    display.innerText = valueTwo ? valueTwo : '0';
+  } else {
+    valueOne = valueOne.slice(0, valueOne.length - 1)
+    display.innerText = valueOne ? valueOne : '0';
+  }
+});
